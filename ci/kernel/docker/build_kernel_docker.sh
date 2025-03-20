16230b853eac8917a8d69d1a9cd19f5e0db76f93
yonatan-ratner <yonaratner@gmail.com>
2025-03-20 10:42:47 +0000

    name updates

--- ci/kernel/docker/build_kernel_docker.sh
+++ ci/kernel/docker/build_kernel_docker.sh
@@ -3,7 +3,7 @@ set -e
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t ghcr.io/$GITHUB_USER/gcc-kmod-builder:latest \
+  -t ghcr.io/$GITHUB_USER/kmod-builder:latest \
   -f "$(dirname "$0")/Dockerfile.kmod" \
   --push "$(git rev-parse --show-toplevel)"
 
